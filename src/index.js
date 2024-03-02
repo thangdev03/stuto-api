ee5f4432a834695bd55f19b8fa6eb990e628de12
thangdev03 <127105305+thangdev03@users.noreply.github.com>
2024-03-02 12:50:11 +0700

    update mongodb url

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("Hello World");
+  return response.status(234).send("This is API of StuTo App - belongs to ThangDev03");
 });
 
 app.use("/major", majorRoute);
